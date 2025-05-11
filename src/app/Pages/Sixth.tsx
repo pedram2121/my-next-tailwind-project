52ac86220d25ffd2b5517d8f404b3e619a41acad
Pedram <pedramsharifi72@gmail.com>
2025-05-11 10:43:11 +0330

    Update

--- src/app/Pages/Sixth.tsx
+++ src/app/Pages/Sixth.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import React from "react";
 import { useResponsive } from "../components/ResponsiveContext";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -24,37 +24,62 @@ const Sixth: React.FC<SixthProps> = ({
 
   const cards = [
     {
-      name: "احسان صالحی",
-      image: "/images/comment_1.png",
+      name: "طلا گروه خرداد",
+      image: "/images/kh.jpg",
+      link: "https://tjmkk.com",
       comment:
-        "از وقتی با آوامین آشنا شدم، دیگه نگران نوسانات قیمت طلا نیستم. اعلان‌های لحظه‌ایش بهم کمک کرد یه خرید عالی داشته باشم!",
+        "با پشتیبانی آوامین، گروه خرداد خرید و فروش طلای آب‌شده رو با اطمینان کامل انجام می‌ده. امنیت پلتفرم و گزارش‌های دقیقشون بی‌نظیره.",
     },
     {
-      name: "مهران غلامی",
-      image: "/images/comment_2.png",
+      name: "طلای مهرشاد",
+      image: "/images/meh.jpg",
+      link: "https://mehrshad.avamin.com",
       comment:
-        "پرداخت‌ها تو آوامین خیلی سریع و امن انجام میشه. برای فروش طلاهام خیلی راحت تونستم اعتماد کنم.",
+        "آوامین با سیستم امن و سریعش به مهرشاد کمک کرد تا تراکنش‌هامون رو با خیال راحت انجام بدیم. واقعاً قابل اعتماده.",
     },
     {
-      name: "حامد رضایی",
-      image: "/images/comment_3.png",
+      name: "طلای فهد",
+      image: "/images/fahd.jpg",
+      link: "https://fahad.avamin.com/",
       comment:
-        "مدیریت سرمایه‌گذاری با آوامین واقعاً ساده‌ست. گزارش‌های دقیقش بهم کمک کرد سود خوبی از خرید طلا ببرم.",
+        "فضایی که آوامین برای طلای فهد فراهم کرده، شفاف و حرفه‌ایه. مشاوره تخصصی و امنیت اطلاعاتمون تضمین‌شده‌ست.",
     },
     {
-      name: "سارا محمدی",
-      image: "/images/comment_2.png",
+      name: "طلای کیوان",
+      image: "/images/k1.jpg",
+      link: "https://keyvan.avamin.com/",
       comment:
-        "آوامین بهترین اپ برای خرید و فروش طلاست. رابط کاربریش خیلی ساده و کاربردیه، همیشه ازش استفاده می‌کنم.",
+        "کیوان به لطف پلتفرم کاربرپسند آوامین تونسته خدمات باکیفیتی ارائه بده. پشتیبانی 24 ساعته‌شون عالیه.",
+    },
+    {
+      name: "طلای زرین در",
+      image: "/images/z1.jpg",
+      link: "https://zarin.avamin.com",
+      comment:
+        "زرین در با حمایت آوامین تجربه‌ای بی‌نظیر از خرید سریع و امن طلا ارائه می‌ده. تکنولوژی پیشرفته‌شون حرف نداره.",
+    },
+    {
+      name: "طلای رز",
+      image: "/images/roz.jpg",
+      link: "https://rozgold.avamin.com/",
+      comment:
+        "طلای رز با کمک آوامین قیمت‌های منصفانه و شفافی ارائه می‌کنه. پلتفرمشون تجارت طلا رو خیلی ساده کرده.",
+    },
+    {
+      name: "طلا وست",
+      image: "/images/vest.jpg",
+      link: "https://talavest.avamin.com",
+      comment:
+        "طلا وست با ابزارهای تحلیل بازار آوامین تونسته سرمایه‌گذاری هوشمندانه‌ای در طلا داشته باشه. امنیتشون بی‌رقیبه.",
     },
   ];
 
   const cardClass = isMobile
-    ? "w-[280px] h-[400px]"
+    ? "w-[280px] h-[420px]"
     : isTablet
-    ? "w-[320px] h-[400px]"
-    : "w-[400px] h-[520px]";
-  const textSize = isMobile ? "text-2xl" : isTablet ? "text-3xl" : "text-4xl";
+    ? "w-[320px] h-[420px]"
+    : "w-[400px] h-[540px]";
+  const textSize = isMobile ? "text-xl" : isTablet ? "text-2xl" : "text-2xl";
   const nameSize = isMobile
     ? "text-[14px]"
     : isTablet
@@ -63,8 +88,8 @@ const Sixth: React.FC<SixthProps> = ({
   const descriptionSize = isMobile
     ? "text-[12px] leading-7"
     : isTablet
-    ? "text-sm leading-5"
-    : "text-base leading-6";
+    ? "text-sm leading-6"
+    : "text-base leading-7";
   const spaceBetween = isMobile ? 10 : isTablet ? 20 : 30;
 
   const breakpoints: SwiperOptions["breakpoints"] = {
@@ -88,7 +113,7 @@ const Sixth: React.FC<SixthProps> = ({
       <h1
         className={`text-4xl font-bold text-black mb-16 mt-12 font-[IRANSans]`}
       >
-        آوامین در نظر مشتریان
+        تجربه برندهای طلا با آوامین
       </h1>
       <div className="w-full max-w-7xl mx-auto relative">
         <div className="w-full">
@@ -115,7 +140,8 @@ const Sixth: React.FC<SixthProps> = ({
             {cards.map((card, index) => (
               <SwiperSlide
                 key={index}
-                className={`swiper-slide-custom ${cardClass} bg-white p-6 rounded-lg shadow-md flex flex-col justify-between transition-all duration-300 cursor-pointer`}
+                className={`swiper-slide-custom ${cardClass} bg-white p-6 rounded-lg shadow-md flex
+                 flex-col justify-between transition-all duration-300 cursor-pointer relative pb-20`}
               >
                 <div className="flex flex-row-reverse items-center mb-6">
                   <Image
@@ -123,7 +149,7 @@ const Sixth: React.FC<SixthProps> = ({
                     alt={card.name}
                     width={60}
                     height={60}
-                    className="w-14 h-14 rounded-full mr-3"
+                    className="w-14 h-14 rounded-full mr-3 hover:scale-110 transition-transform duration-200"
                   />
                   <h2
                     className={`${nameSize} mr-4 font-semibold text-purple-800 font-[IRANSans]`}
@@ -132,10 +158,18 @@ const Sixth: React.FC<SixthProps> = ({
                   </h2>
                 </div>
                 <p
-                  className={`${descriptionSize} text-gray-700 text-right font-[IRANSans] line-clamp-8 text-justify-last-right`}
+                  className={`${descriptionSize} text-gray-700 text-right font-[IRANSans] line-clamp-8 text-justify‍‌justify-last-right`}
                 >
                   {card.comment}
                 </p>
+                <a
+                  href={card.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="absolute bottom-4 left-4 bg-purple-600 text-white text-sm px-4 py-2 rounded-full hover:bg-purple-700 transition-colors duration-200 hover:shadow-lg font-[IRANSans]"
+                >
+                  بازدید از سایت
+                </a>
               </SwiperSlide>
             ))}
           </Swiper>
@@ -149,7 +183,7 @@ const Sixth: React.FC<SixthProps> = ({
       <h1
         className={`text-3xl font-bold text-black mb-16 mt-12 font-[IRANSans]`}
       >
-        آوامین در نظر مشتریان
+        تجربه برندهای طلا با آوامین
       </h1>
       <div className="w-full max-w-7xl mx-auto relative">
         <div className="w-full">
@@ -176,7 +210,7 @@ const Sixth: React.FC<SixthProps> = ({
             {cards.map((card, index) => (
               <SwiperSlide
                 key={index}
-                className={`swiper-slide-custom ${cardClass} bg-white p-6 rounded-lg shadow-md flex flex-col justify-between transition-all duration-300 cursor-pointer`}
+                className={`swiper-slide-custom ${cardClass} bg-white p-6 rounded-lg shadow-md flex flex-col justify-between transition-all duration-300 cursor-pointer relative`}
               >
                 <div className="flex flex-row-reverse items-center mb-6">
                   <Image
@@ -184,7 +218,7 @@ const Sixth: React.FC<SixthProps> = ({
                     alt={card.name}
                     width={60}
                     height={60}
-                    className="w-10 h-10 rounded-full mr-3"
+                    className="w-10 h-10 rounded-full mr-3 hover:scale-110 transition-transform duration-200"
                   />
                   <h2
                     className={`${nameSize} mr-4 font-semibold text-purple-800 font-[IRANSans]`}
@@ -197,6 +231,14 @@ const Sixth: React.FC<SixthProps> = ({
                 >
                   {card.comment}
                 </p>
+                <a
+                  href={card.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="absolute bottom-4 left-4 bg-purple-600 text-white text-sm px-4 py-2 rounded-full hover:bg-purple-700 transition-colors duration-200 hover:shadow-lg font-[IRANSans]"
+                >
+                  بازدید از سایت
+                </a>
               </SwiperSlide>
             ))}
           </Swiper>
@@ -206,12 +248,13 @@ const Sixth: React.FC<SixthProps> = ({
   );
 
   const renderMobile = () => (
-    <div className="flex flex-col items-center justify-center h-[550px] py-8 px-4 bg-[#F4FEFF] overflow-x-hidden">
-      <h1 className={`text-2xl font-bold text-black mb-14 font-[IRANSans]`}>
-        آوامین در نظر مشتریان
+    <div className="flex flex-col items-center justify-center min-h-[600px] py-8 px-4 bg-[#F4FEFF] overflow-x-hidden">
+      <h1
+        className={`text-2xl font-bold text-black mb-14 font-[IRANSans]`}>
+        تجربه برندهای طلا با آوامین
       </h1>
       <div className="w-full max-w-7xl mx-auto relative flex justify-center">
-        <div className="w-full h-auto">
+        <div className="w-full">
           <Swiper
             dir={direction}
             modules={[Autoplay, Pagination, Navigation]}
@@ -230,32 +273,42 @@ const Sixth: React.FC<SixthProps> = ({
             }}
             navigation={false}
             breakpoints={breakpoints}
-            className={`pb-24 ${className}`}
-          >
+            className={`pb-24 ${className}`}>
+         
             {cards.map((card, index) => (
               <SwiperSlide
                 key={index}
-                className={`swiper-slide-custom ${cardClass} bg-white p-5 rounded-lg shadow-md flex flex-col justify-between transition-all duration-300 cursor-pointer`}
-              >
+                className={`swiper-slide-custom ${cardClass} bg-white p-5 rounded-lg shadow-md 
+                flex flex-col justify-between transition-all duration-300 cursor-pointer relative pb-20`}>
+             
                 <div className="flex flex-row-reverse items-center mb-4 h-32">
                   <Image
                     src={card.image}
                     alt={card.name}
                     width={60}
                     height={60}
-                    className="w-14 h-14 rounded-full mr-3"
-                  />
+                    className="w-14 h-14 rounded-full mr-3 hover:scale-110 transition-transform duration-200"/>
+                  
                   <h2
-                    className={`${nameSize} mr-4 font-semibold text-purple-800 font-[IRANSans]`}
-                  >
+                    className={`${nameSize} mr-4 font-semibold text-purple-800 font-[IRANSans]`}>
                     {card.name}
                   </h2>
+
                 </div>
-                <p
-                  className={`${descriptionSize} text-gray-700 text-right font-[IRANSans] line-clamp-8 text-justify-last-right`}
-                >
+
+                <p className={`${descriptionSize} text-gray-700 text-right font-[IRANSans] line-clamp-8 text-justify-last-right -mt-6`}>
                   {card.comment}
                 </p>
+
+                <a
+                  href={card.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="absolute bottom-4 left-4 bg-purple-600 text-white text-sm px-4 py-2 
+                  rounded-full hover:bg-purple-700 transition-colors duration-200 hover:shadow-lg font-[IRANSans]">
+                  بازدید از سایت
+                </a>
+
               </SwiperSlide>
             ))}
           </Swiper>
